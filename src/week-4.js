3a50397923c788ec4e5780b03af11e36312cd36c
NewFacesV2 <Iain-Henderson@live.nmit.ac.nz>
2022-03-16 10:14:34 +1300

    changed week-4.js

--- src/week-4.js
+++ src/week-4.js
@@ -10,4 +10,33 @@ var classCode = "503";
 console.log(fullName)
 console.log(classCode)
 
+// Javascript Objects
 
+var full_name =
+{
+first_name: "Iain",
+second_name: "Henderson",
+course_number: 503
+}
+
+
+console.log(full_name.first_name)
+console.log(full_name["second_name"])
+
+// Javascript Functions
+
+function addTwoNumbers(num1, num2) {
+return num1+num2
+}
+
+console.log(addTwoNumbers(12,3))
+
+function addThreeNumbers(num1, num2, num3) {
+    return num1+num2+num3
+
+
+}
+
+console.log(addThreeNumbers(100,200,300))
+
+//var num1 = 2
