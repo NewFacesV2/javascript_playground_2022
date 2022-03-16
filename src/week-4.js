41aaa002beb68ca0449d7cb599f808589b1b06cf
NewFacesV2 <Iain-Henderson@live.nmit.ac.nz>
2022-03-16 15:00:42 +1300

    lesson finished

--- src/week-4.js
+++ src/week-4.js
@@ -52,5 +52,120 @@ if (number === 10) {
     console.log("error")
 }
 
+// Javascript comments
+
+/*
+Hello world
+class sdv503
+
+*/
+
+var sdv503 = "Javascript"
+
+var x = 3
+
+var fullName1 = "text"
+
+//foo(x,y)
+
+// function foo(p1,p2) {
+// return p2 - p1
+// }
+
+// foo(5,10)
+
+//  var new_value = function sum(x1, x2) {
+//     return x1 + x2
+// }
+
+// sum(10, 3)
+
+
+// (=) vs (===)
+
+//statements vs expressions
+var numbers 
+
+//statement
+// var x
+// if (y >=0) {
+//     x = y
+// } else {
+//     x = -y
+// }
+// function statements(x,y) {
+//     if(y >=0) {
+//         return x = y
+//     } else {
+//         return x = -y
+//     }
+// }
+
+//expression ES6
+//var x = y >= 0 ? y : -y
+
+// var word = "abcdefghijklmnopqrstuvwxyz"
+// console.log(word.length)
+
+// var obj = {}
+// console.log(obj)
+
+// obj.foo = 1234
+// console.log(obj)
+// console.log(word.toUpperCase())
+
+// Javascript Values (primitive vs objects)
+
+// var obj1 = {}
+// var obj2 = {}
+
+// console.log(obj1 === obj2)
+
+// var number_1 = 123
+// var number_2 = 123
+
+// console.log(number_1 === number_2)
+
+// boolean: True and false
+// Numbers: 123  4.1  415.65
+// strings: "hello", or 'hello'
+// non values: null and undefined
+
+// var str = "Henderson"
+// console.log(str.length = 2)
+// console.log(str.length)
+
+// var number = [1,2,3,4,5]
+// console.log(number [3] = 10)
+// console.log(number)
+
+// ({} === {})
+// console.log({} === {})
+
+// //non values
+
+// var nonvalue
+// console.log(nonvalue)
+
+// function nonvalue(x) {
+//     return x
+// }
+
+// console.log(nonvalue())
+
+var value = function(){}
+console.log(typeof value)
+
+//false, 0, '', NaN, false, [],
+
+
+
+
+
+ 
+
+
+
+
 
 
