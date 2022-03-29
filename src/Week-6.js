97b9385317f5bb399fc653ac86829cc02b72bc2b
NewFacesV2 <Iain-Henderson@live.nmit.ac.nz>
2022-03-30 11:45:50 +1300

    changes to week-6.js

--- src/Week-6.js
+++ src/Week-6.js
@@ -4,3 +4,29 @@
 // console.log("Hello world")
 // console.error("Hello world")
 // console.dir(document)
+
+// console.log("hello world");
+// var a = "hello";
+// console.log(a);
+
+// var myName = "Iain"
+// var myCourse = "Javascript"
+// console.log(myName)
+// var myAge = 25
+// console.log(myAge)
+// alert("Welcome");
+/* Comment */
+
+var welcome = "Welcome"  //NOTE This is declaring a variable "welcome" and assigning the value "Welcome"
+// console.log(welcome)  //NOTE this would have printed the original variable value "welcome", I have blanked this line out for now
+var welcome = "Welcome to the gulag" //NOTE this line uses the same variable "Welcome" but I have changed the value to "Welcome to the gulag"
+console.log(welcome) //NOTE This line will print the variable "welcome" with the latest assigned value
+
+var myName = "iain" //NOTE declaring a variable "myName" an assigning the value "Iain"
+var myName = "ali" //NOTE Changing the value of myName to "ali"
+console.log(myName) //NOTE this will print the latest assigned value of "myName", which is currently "ali"
+
+
+
+
+
