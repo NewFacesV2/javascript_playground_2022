4b2ecb571a21b03f53e7c8072a4f7be159682d59
NewFacesV2 <Iain-Henderson@live.nmit.ac.nz>
2022-03-30 08:57:19 +1300

    new working week branch

---
+++ src/Homepractice.js
@@ -0,0 +1,27 @@
+//NOTE Practicing
+
+// var mynumber = 1
+
+// console.log(mynumber)
+
+// var mylocation = 'nelson', myotherlocation = 'Abraham Heights';
+
+// mylocation === myotherlocation;
+// console.log(mylocation === myotherlocation)
+
+// var myotherlocation = 'nelson'
+// console.log(mylocation === myotherlocation)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
