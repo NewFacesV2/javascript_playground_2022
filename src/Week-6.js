8623e47816b8c9b00946e062fa3c4104805a112f
NewFacesV2 <Iain-Henderson@live.nmit.ac.nz>
2022-04-01 16:02:41 +1300

    more work on week-6.js

--- src/Week-6.js
+++ src/Week-6.js
@@ -89,18 +89,171 @@
 
 //console.log(msg)
 
-function outerfunc() {
-    var msg = "hello world"
+// function outerfunc() {
+//     var msg = "hello world"
 
-function innerfunc() {
-    console.log(msg)
+// function innerfunc() {
+//     console.log(msg)
+// }
+
+// console.log(innerfunc())
+// }
+
+// console.log(outerfunc())
+
+//NOTE DATA TYPES
+
+/*
+1 - Numbers
+2 - Booleans
+3 - Strings
+4 - Objects
+5 - 
+
+*/
+
+// let a = 1 // Number Datatype
+// let b = "Hello" // B is a string
+// let c = true // C is a boolean datatype
+// let d; // D is undefined Datatype
+
+// let sum = 1 + 1
+// console.log(sum)
+
+// let num = 3
+// console.log(num)
+
+// let test = 100 / "2"
+// console.log(test)
+
+
+// const value1 = '5';
+// const value2 = 9;
+// let sum = Number(value1) + value2;
+
+// console.log(sum);
+
+// let output = `the sum of ${value1} and ${value2} is ${sum}`
+// console.log(output)
+
+// let alphabet = '4'
+// console.log(Number(alphabet));
+
+// let Str = "Iain Henderson"
+// console.log(Str)
+// let Str1 = "nmit"
+// // console.log(Str1)
+// // let fullStr = Str + " " +  Str1
+// // console.log(fullStr)
+
+// // let newStr = `Hello to my new string! My name is ${Str} and I Study at ${Str1}`
+
+// // console.log(newStr)
+
+// //Boolean only two keywords true and false
+
+// // let bol = true
+// // console.log(bol)
+
+// // console.log(3.01 > 3)
+// // >= or <= or == or !=
+
+// // console.log( 5 == 5.0)
+// // console.log(9 < 8)
+// // console.log(3 > 4)
+
+// // let test = ["Hello", "Ali", 21, 2013] // Array Datatype
+// // // console.log(test)
+// // // console.log(test[2])
+// // for(i=0;i<test.length; i++) {
+// //     console.log(test[i])
+// // }
+
+// //
+
+// // var cat = {
+// //     fur: 'black',
+// //     whiskers: 'many',   //Object
+// //     paws: '5',
+// // }
+
+// // console.log(cat)
+
+// //NOTE Numbers
+
+// let sum = 20 + 50
+// console.log(sum)
+
+
+// let total = '10' / 2
+// console.log(total)
+
+// //NOTE Strings
+
+// let a = 'basil'
+// let b = 'tuxedo'
+
+// let catType = `My cats name is ${a} he is a ${b} cat`
+// console.log(catType)
+
+// let make = 'toyota'
+// let model = 'vitz'
+// let katiesCar = `Katie drives a ${make} ${model}`
+// console.log(katiesCar)
+
+// //NOTE Booleans
+
+// console.log(12 >= 7)
+// console.log(12 >= 15)
+// console.log(50 == 50)
+// console.log(5 === 5.0)
+
+// //NOTE Arrays
+
+// let dinner = ["chicken", 'curry', 'brocolli']
+// console.log(dinner[2])
+
+// let pets = ['aspen', 'basil', 'chickens']
+// for (i=0; i<pets.length; i++) {
+//     console.log(pets[i])
+// }
+
+
+//NOTE Objects
+
+
+
+
+// A function is a block of code designed to perform a particular set of tasks.  Syntax = Function function1() {}
+
+// function div(num1, num2) {
+//     return num1 - num2
+// }
+
+// console.log(div(3,10))
+
+// function sum1(num1, num2, num3, num4) {
+//     return num1 + num2 + num3 + num4;
+// }
+
+// let num1 = 4
+// let num2 = 5
+// let num3 = 6
+// let num4 = 7
+
+// total1 = `the total is ${num1} + ${num2} + ${num3} + ${num4}`
+
+// console.log(sum1)
+
+function fourNumbers(num1, num2, num3, num4) {
+    let result = num1 + num2 + num3 + num4
+    return `the sum of 4 numbers is ${result}`
 }
 
-console.log(innerfunc())
-}
+console.log(fourNumbers(1,2,3,4))
 
-console.log(outerfunc())
+function test1() {}
 
-
+console.log(test1())
 
 
