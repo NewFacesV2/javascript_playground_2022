e4243d6572c029be86c52a9a5b91c3764033dce9
katienordstrom <katie-nordstrom@live.nmit.ac.nz>
2022-04-06 09:31:27 +1200

    First pair code week-7

--- src/Week-7.js
+++ src/Week-7.js
@@ -1,1 +1,18 @@
+//Iain and Katie Pair Coding
 //hello its me
+
+
+// var array1 = [12, 22, 33, 44, 55] //declared a variable array1, assigned the values of the array
+
+// function function1(arr) { //declared a function called function1
+//     return arr[0]
+// }
+// console.log(array1([1, 2]))
+
+
+
+
+function getArrElement(a) {
+    return arr[0]
+}
+console.log(getArrElement([1,2,3,4]))
