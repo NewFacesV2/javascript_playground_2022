171b8fff49f13bda24da157a18ac66748dafc2ba
NewFacesV2 <Iain-Henderson@live.nmit.ac.nz>
2022-04-06 11:53:52 +1200

    done for the morning

--- src/Week-7.js
+++ src/Week-7.js
@@ -22,6 +22,57 @@
 
 //Iain test comment
 
+// function reverseArray(arr) {
+
+//     return arr.reverse()
+
+// }
+
+// console.log(reverseArray([1,2,3,4]))
+
+
+//create a function that accepts an array and return the last element
+//example lastelement(["ali", 12, "blue", "green"])
+
+// function lastelement(arr) {
+//     return arr.pop()
+// }
+
+// console.log(lastelement(["Ali", 12, "blue", "green"]))
+
+//create a function that creates an array and returns as a string
+
+// function tostringarray(arr) {
+//     return arr.toString()
+// }
+
+// const result = tostringarray(["ali", 2, 3, 4, "green"])
+
+// console.log(typeof result)
+
+// function ArrayToString(arr) {
+//     return arr.join('')
+// }
+
+// console.log(ArrayToString(["ali ", 12, " blue"]))
+
+//create a function that takes an array and return the data types of the array values in a new array
+// arrayValueTypes(["ali", 12, "blue", [], true]) -> String, number, string, object, booLean]
+
+// function arrayValueTypes(arr) {
+//     var newArray = [];
+
+//     for(var i = 0; i < arr.length; i++) {
+//         newArray[i] = typeof arr[i]
+//     }
+    
+//     return newArray
+// }
+
+// const arrayValueTypes = arr => arr.map(x => typeof x)
+
+
+
 
 
 
